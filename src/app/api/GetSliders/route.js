81bd6853a2202ae88baf74c96caffb3eb3c26b2f
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-01 11:51:10 +0330

    Fetch api

---
+++ src/app/api/GetSliders/route.js
@@ -0,0 +1,12 @@
+import { supabase } from "@/app/utils/client";
+
+export async function GET() {
+  try {
+    let { data: products, error } = await supabase
+      .from("tbl_slider")
+      .select("*");
+    return Response.json(products, { status: 200 });
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
