3de557b9c1c55aeb78f4660bb623bf4df2227240
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-08 02:17:26 +0330

    Fetch product data

---
+++ src/app/utils/ProductService.js
@@ -0,0 +1,19 @@
+export const getProduct = async (input_params) => {
+  try {
+    const response = await fetch(
+      `https://adminhydrogenous.vercel.app/api/GetShipment?${input_params}`
+    );
+
+    if (!response.ok) {
+      console.error("Failed to fetch:", response.status);
+      return null;
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching :", error);
+    return null;
+  }
+};
